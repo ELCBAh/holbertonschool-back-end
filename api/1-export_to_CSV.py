af060e3018b2568e90928073fa231a4e633cb18d
ELCBAh <6211@holbertonstudents.com>
2023-08-27 18:43:33 +0000

    fixed files not found because of missing structure

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Lists all tasks from a given employee ID in CSV format."""
+import requests, sys, csv
+
+
+if __name__ == "__main__":
+    usr_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+
+    completed = [todo for todo in todos if todo.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed),
+                                                          len(todos)))
+    [print("\t {}".format(todo.get("title"))) for todo in completed]

6568385e9aac61f53d171b90a19f275740b55026
ELCBAh <6211@holbertonstudents.com>
2023-08-27 19:16:32 +0000

    rolling back version to change approach again, no errors when executing but full of errors in checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,25 +2,19 @@
 """Lists all tasks from a given employee ID."""
 import requests
 import sys
-
-
-def main():
-    """Prints info about employee"""
-    user_id = sys.argv[1]
-    user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-        user_id)
-    user_name = requests.get(user).json().get('name')
-    request_todo = requests.get(todos).json()
-    tasks = [task.get('title')
-             for task in request_todo if task.get('completed') is True]
-
-    print('Employee {} is done with tasks({}/{}):'.format(user_name,
-                                                          len(tasks),
-                                                          len(request_todo)))
-    print('\n'.join('\t {}'.format(task) for task in tasks))
+import json
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        main()
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    completed = [todo for todo in todos if todo.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+                                                          len(completed),
+                                                          len(todos)))
+    [print("\t {}".format(todo.get("title"))) for todo in completed]

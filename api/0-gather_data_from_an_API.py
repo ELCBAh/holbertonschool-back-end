c73dd861deab531aa1173fa11e7807eceedaa14e
ELCBAh <6211@holbertonstudents.com>
2023-08-27 18:57:06 +0000

    fixing naming errors

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [todo for todo in todos if todo.get("completed") is True]
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
                                                           len(completed),
                                                           len(todos)))

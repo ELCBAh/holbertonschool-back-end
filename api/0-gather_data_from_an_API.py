03260951b2dbc0072ab5c8b0e24ad6303ba311ee
ELCBAh <6211@holbertonstudents.com>
2023-08-27 19:04:02 +0000

    trying a different approach to fix checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,12 +5,16 @@ import sys
 
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
+    user_id = sys.argv[1]
+    user = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    todos = "https://jsonplaceholder.typicode.com/todos/?userId={}".format(
+        user_id)
+    user_info = requests.get(user).json()
+    user_name = user_info.get("username")
+    todo_list = requests.get(todos).json()
+    completed = [todo for todo in todo_list if todo.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".format(user_name,
                                                           len(completed),
-                                                          len(todos)))
-    [print("\t {}".format(todo.get("title"))) for todo in completed]
+                                                          len(todo_list)))
+    for todo in completed:
+        print("\t {}".format(todo.get("title")))

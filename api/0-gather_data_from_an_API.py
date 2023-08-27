c17bb50adcd6500f42819c84ebd8fcdc2e9c83fe
ELCBAh <6211@holbertonstudents.com>
2023-08-27 19:27:44 +0000

    code should be working as expected

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,12 +6,11 @@ from django.conf.urls import url
 
 
 if __name__ == "__main__":
-    """module not documented error"""
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    username = user.get("username")
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [todo for todo in todos if todo.get("completed")]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(completed),
-                                                          len(todos)))
+
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+    [print("\t {}".format(c)) for c in completed]

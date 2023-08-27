d8123ad36884905a379582c43c5b856e90eb36bb
ELCBAh <6211@holbertonstudents.com>
2023-08-27 19:19:35 +0000

    trying a different approach, code breaking only when using checker

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,19 +2,30 @@
 """Lists all tasks from a given employee ID."""
 import requests
 import sys
-import json
 
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         sys.exit(1)
 
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
-    completed = [todo for todo in todos if todo.get("completed")]
-    print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-                                                          len(completed),
-                                                          len(todos)))
-    [print("\t {}".format(todo.get("title"))) for todo in completed]
+    user_id = int(sys.argv[1])
+    todos_url = "https://jsonplaceholder.typicode.com/todos?userId=".format(user_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/".format(user_id)
+    todos_response = requests.get(todos_url)
+    todos_data = todos_response.json()
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    completed_tasks = 0
+    total_tasks = 0
+    count_tasks = 0
+    employee_name = user_data.get("name")
+    for todo in todos_data:
+        if todo["userId"] == user_id:
+            count_tasks += 1
+            if todo["completed"] is True:
+                completed_tasks += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_tasks, count_tasks))
+    for todo in todos_data:
+        if todo["userId"] == user_id and todo["completed"] is True:
+            print("\t {}".format(todo["title"]))
